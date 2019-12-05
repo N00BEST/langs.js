e5fbfafccd31d91a512e0dcad9c952841ab5ca64
N00BEST <marcano.daniel98@gmail.com>
2019-12-05 00:16:16 -0500

    Added the translator core and an example of how translations and langs files work

---
+++ source/es.js
@@ -0,0 +1,17 @@
+var LANGS = LANGS !== undefined ? LANGS : [];
+
+LANGS['es'] = {
+    'customer': {
+        'title': 'Cliente',
+        'name': 'Nombre', 
+        'login': 'Iniciar Sesion'
+    },
+    'website': 'Sitio Web',
+    'welcome-to': 'Bienvenido a :page!', 
+    'title': 'Titulo',
+    'site': {
+        'errors': {
+            'not-found': 'No encontrado'
+        }
+    }
+};
